38ee01ad446fae2212f5307d7757e49432776f34
Gilang Ramadan <mgrware@gmail.com>
2019-12-19 10:35:33 +0700

    add websocket

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"net/http"
-	"mongo/controllers/articles"
 	"github.com/gin-gonic/gin"
   "mongo/db"
-  "mongo/middlewares"
+  "github.com/gorilla/websocket"
+  "log"
+
 )
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message)           // broadcast channel
 
 func init() {
 	db.Connect()
+}
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
 }
 
 func setupRouter() *gin.Engine {
@@ -17,17 +27,7 @@ func setupRouter() *gin.Engine {
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 
-	r.Use(middlewares.Connect)
-	r.Use(middlewares.ErrorHandler)
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	r.GET("articles", articles.Index)
-	r.POST("articles", articles.Create)
-	r.POST("articles/:_id", articles.Update)
-	r.GET("articles/:_id", articles.Edit)
+  r.GET("/ws", gin.WrapF(handleConnections))
 
 
 	return r
@@ -37,5 +37,56 @@ func main() {
 
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
+	go handleMessages()
 	r.Run(":8080")
+}
+
+var wsupgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Upgrade initial GET request to a websocket
+	ws, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	// Register our new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+		// Send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
